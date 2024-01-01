8c8abd0d13a1f78e1261d0b142f48e4ae1e19377
suijt1984 <sujitsugaathan2002@gmail.com>
2024-01-01 22:07:30 +0530

    Added docstrings to the custom functions

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-def get_todo_tasks(filepath):
+def get_todo_tasks(filepath="todo_list.txt"):
+    """Read the current content of the todo_list.txt file and return its contents"""
     with open(filepath, 'r') as local_file:
         local_todos = local_file.readlines()
     return local_todos
 
-def set_todo_tasks(filepath, todos_arg):
+def set_todo_tasks(todos_arg,filepath="todo_list.txt"):
+    """Write the new or updated tasks to a file todo_list.txt"""
     with open(filepath, 'w') as local_file:
         local_file.writelines(todos_arg)
 
-# def set_todo_tasks():
-#     with open("./todo_list.txt", 'w') as file:
-#         todos = file.writelines(todos)
 
 while True:
     user_action = input("Type add or show or edit or complete or exit ")
@@ -18,14 +17,14 @@ while True:
     if user_action.startswith('add') or user_action.startswith('new'):
             #todo = input("Enter a todo:") + '\n'
             todo = user_action[4:]
-            todos = get_todo_tasks("todo_list.txt")
+            todos = get_todo_tasks()
 
             todos.append(todo + '\n')
-            set_todo_tasks("todo_list.txt", todos)
+            set_todo_tasks(todos)
 
     elif user_action.startswith('show'):
             #file = open("./todo_list.txt",'r')
-        todos= get_todo_tasks("todo_list.txt")
+        todos= get_todo_tasks()
             #file.close()
             #new_todos = [item.strip('\n') for item in todos]
         for index, item in enumerate(todos):
@@ -37,13 +36,13 @@ while True:
             number = int(user_action[5:])
             number = number - 1
 
-            todos = get_todo_tasks("todo_list.txt")
+            todos = get_todo_tasks()
                 #print("Todos before the modification",todos)
 
             new_task = input('Enter the new task:')
             todos[number] = new_task + '\n'
 
-            set_todo_tasks("todo_list.txt",todos)
+            set_todo_tasks(todos)
 
         except ValueError:
             print("Your command is not valid:")
@@ -53,9 +52,9 @@ while True:
         try:
             num = int(user_action[9:])
             del_num = num - 1
-            get_todo_tasks("todo_list.txt")
+            get_todo_tasks()
             todos.pop(del_num)
-            set_todo_tasks("todo_list.txt",todos)
+            set_todo_tasks(todos)
 
         except IndexError:
             print(f"There is no task with the number {num}")
