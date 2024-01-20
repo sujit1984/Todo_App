49e8f663f22b88db948ece6bd49919c91b7cda6e
suijt1984 <sujitsugaathan2002@gmail.com>
2024-01-20 21:46:29 +0530

    Add the changes in the gui.py for the Edit button functionality

--- gui.py
+++ gui.py
@@ -5,20 +5,36 @@ import PySimpleGUI as sg
 label = sg.Text("Enter the Task")
 input_box = sg.InputText(tooltip="Enter the todo task", key = "Task")
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=get_todo_tasks(), key="Task_List",
+                      enable_events=True, size=[45, 10])
+edit_button = sg.Button("Edit")
+
 window = sg.Window("To Do App",
-                   layout = [[label],[input_box, add_button]],
-                   font=('Helvetica', 20))
+                   layout=[[label],[input_box, add_button],[list_box, edit_button]],
+                   font=('Helvetica', 16))
 
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
+    print(1,event)
+    print(2, values)
+    print(3, values["Task_List"])
     match event:
         case "Add":
             todos = get_todo_tasks()
             new_task = values["Task"] + "\n"
             todos.append(new_task)
             set_todo_tasks(todos)
+            window['Task_List'].update(values=todos)
+        case "Edit":
+            task_to_edit = values["Task_List"][0]
+            new_task = values["Task"]
+            tasks = get_todo_tasks()
+            index = tasks.index(task_to_edit)
+            tasks[index] = new_task
+            set_todo_tasks(tasks)
+            window['Task_List'].update(values=tasks)
+        case "Task_List":
+            window['Task'].update(value=values['Task_List'][0])
         case sg.WIN_CLOSED:
             break
 
