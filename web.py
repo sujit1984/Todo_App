e0b8a0c339fdb742e18c81fb69899ff9a986a1e2
suijt1984 <sujitsugaathan2002@gmail.com>
2024-02-24 21:06:05 +0530

    Implement the code for Completing a task using the web app

--- web.py
+++ web.py
@@ -12,8 +12,14 @@ st.title("My Daily Tasklist")
 
 #st.checkbox("First task")
 
-for task in tasks:
-    st.checkbox(task)
+for index, task in enumerate(tasks):
+    check_box = st.checkbox(task, key=task)
+    if check_box:
+        tasks.pop(index)
+        methods.set_todo_tasks(tasks)
+        del st.session_state[task]
+        st.experimental_rerun()
+
 
 st.text_input(label="Add a new task", placeholder="New task",
               on_change=add_task, key="new_task")
